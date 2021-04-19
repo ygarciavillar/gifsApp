62a2f5291812a81e72440326ec7263c381161bfd
Yoandri Garcia <ygarciavillar@gmail.com>
2021-04-19 17:23:31 -0500

    guardando las busquedas en localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,18 +15,23 @@ export class GifsService {
         return [...this._historial];
     }
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+        this.resultado = JSON.parse(localStorage.getItem('resultado')!) || []
+    }
 
     buscarGifs(query: string = '') {
         query = query.trim().toLowerCase()
         if (!this._historial.includes(query)) {
             this._historial.unshift(query)
             this._historial = this._historial.splice(0, 10)
+            localStorage.setItem('historial', JSON.stringify(this._historial))
         }
 
         this.http.get<SearchGIFResponce>(`https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${query}&limit=10`)
             .subscribe(resp => {
                 this.resultado = resp.data
+                localStorage.setItem('resultado', JSON.stringify(this.resultado))
             })
 
     }
